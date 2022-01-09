9592bd5648a10e7b0f4c243e25728fb1c864574c
Haim Ashkenazi <haim@babysnakes.org>
2022-01-10 00:41:33 +0200

    Runtime Environment

---
+++ asdfw/src/runtime.rs
@@ -0,0 +1,26 @@
+use anyhow::{anyhow, Result};
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub struct RuntimeEnvironment {
+    pub current_dir: PathBuf,
+    pub home_dir: PathBuf,
+    pub shims_db: PathBuf,
+    pub installs_dir: PathBuf,
+}
+
+impl RuntimeEnvironment {
+    pub fn new() -> Result<Self> {
+        let home_dir = dirs::home_dir().ok_or(anyhow!("Could not get home directory"))?;
+        let current_dir = std::env::current_dir()?;
+        let app_dir = home_dir.join(".asdfw");
+        let shims_db = app_dir.join("shims.db");
+        let installs_dir = app_dir.join("installs");
+        Ok(RuntimeEnvironment {
+            home_dir,
+            current_dir,
+            shims_db,
+            installs_dir,
+        })
+    }
+}
